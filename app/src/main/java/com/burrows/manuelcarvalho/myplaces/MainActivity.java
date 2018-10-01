882955d3c79b9a4146677d776fa512377b3c4184
AndroidOS <burrowsgit@gmail.com>
2018-10-01 20:40:49 +0700

    Added intent for map

--- app/src/main/java/com/burrows/manuelcarvalho/myplaces/MainActivity.java
+++ app/src/main/java/com/burrows/manuelcarvalho/myplaces/MainActivity.java
@@ -1,5 +1,6 @@
 package com.burrows.manuelcarvalho.myplaces;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -48,6 +49,11 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, MapsActivity.class);
+
+//            String message = editText.getText().toString();
+//            intent.putExtra(EXTRA_MESSAGE, message);
+            startActivity(intent);
             return true;
         }
 
